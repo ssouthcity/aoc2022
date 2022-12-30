26dc484bb56384145ed4655c6b1451d251f7cde8
Stian Sørby <ssouthcity@gmail.com>
2022-12-30 12:50:41 +0100

    optimized day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,135 +1,52 @@
-use std::str::FromStr;
-
 use aoc2022::day;
 
-#[derive(Copy, Clone)]
-enum Shape {
-    Rock,
-    Paper,
-    Scissors,
+fn parse_rps(input: &str) -> Vec<(usize, usize)> {
+    input
+        .lines()
+        .map(|l| {
+            let (a, b) = l.split_once(' ').unwrap();
+
+            let opponent = "ABC".find(a).unwrap();
+            let player = "XYZ".find(b).unwrap();
+
+            (opponent, player)
+        })
+        .collect()
 }
 
-impl Shape {
-    fn index(self) -> usize {
-        self as usize
+fn one(matches: Vec<(usize, usize)>) -> usize {
+    let mut sum = 0;
+
+    for (opponent, player) in matches {
+        let mut results = [3, 6, 0];
+        results.rotate_right(opponent);
+
+        let result = results[player];
+
+        sum += (player + 1) + result;
     }
 
-    fn points(self) -> usize {
-        self.index() + 1
-    }
+    sum
 }
 
-impl FromStr for Shape {
-    type Err = String;
+fn two(matches: Vec<(usize, usize)>) -> usize {
+    let mut sum = 0;
+    let scores = [0, 3, 6];
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" | "X" => Ok(Self::Rock),
-            "B" | "Y" => Ok(Self::Paper),
-            "C" | "Z" => Ok(Self::Scissors),
-            _ => Err(format!("'{}' is not a valid shape", s)),
-        }
-    }
-}
+    for (opponent, result) in matches {
+        let mut results = [3, 1, 2];
+        results.rotate_left(opponent);
 
-#[derive(Clone, Copy)]
-enum MatchResult {
-    Draw,
-    Victory,
-    Defeat,
-}
+        let player = results[result];
 
-impl MatchResult {
-    fn index(self) -> usize {
-        self as usize
+        sum += scores[result] + player
     }
 
-    fn points(self) -> usize {
-        match self {
-            Self::Defeat => 0,
-            Self::Draw => 3,
-            Self::Victory => 6,
-        }
-    }
-}
-
-impl FromStr for MatchResult {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "X" => Ok(MatchResult::Defeat),
-            "Y" => Ok(MatchResult::Draw),
-            "Z" => Ok(MatchResult::Victory),
-            _ => Err(format!("'{}' is not a valid match result", s)),
-        }
-    }
-}
-
-struct Match(Shape, Shape);
-
-impl Match {
-    fn score(&self) -> usize {
-        let mut results = [MatchResult::Draw, MatchResult::Victory, MatchResult::Defeat];
-        results.rotate_right(self.0.index());
-
-        let result = results[self.1.index()];
-
-        self.1.points() + result.points()
-    }
-}
-
-impl FromStr for Match {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (a, b) = s.split_once(" ").expect("unable to parse match");
-
-        Ok(Self(Shape::from_str(a)?, Shape::from_str(b)?))
-    }
-}
-
-struct PartialMatch(Shape, MatchResult);
-
-impl PartialMatch {
-    fn score(&self) -> usize {
-        let mut results = [Shape::Rock, Shape::Paper, Shape::Scissors];
-        results.rotate_left(self.0.index());
-
-        let result = results[self.1.index()];
-
-        result.points() + self.1.points()
-    }
-}
-
-impl FromStr for PartialMatch {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (a, b) = s.split_once(" ").expect("unable to parse partial match");
-
-        Ok(Self(Shape::from_str(a)?, MatchResult::from_str(b)?))
-    }
-}
-
-fn parse_matches(input: &str) -> Vec<Match> {
-    input.lines().map(|l| l.parse().unwrap()).collect()
-}
-
-fn parse_partial_matches(input: &str) -> Vec<PartialMatch> {
-    input.lines().map(|l| l.parse().unwrap()).collect()
-}
-
-fn one(matches: Vec<Match>) -> usize {
-    matches.iter().map(|m| m.score()).sum()
-}
-
-fn two(matches: Vec<PartialMatch>) -> usize {
-    matches.iter().map(|m| m.score()).sum()
+    sum
 }
 
 day!(
     "Rock Paper Scissors",
-    one << parse_matches,
-    two << parse_partial_matches
+    one << parse_rps,
+    two << parse_rps // two << parse_partial_matches
 );
